4fa1eb296152086cb1a00d4db67174b701d69e78
Ned Itsgrieff <empty-trash@outlook.com>
2017-09-23 18:32:33 +0100

    fix java syntax

--- assets/js/bg.js
+++ assets/js/bg.js
@@ -3,15 +3,15 @@ document.addEventListener("DOMContentLoaded", function(){
 	var n = d.getDay();
 	if (n = 0)
 	  document.body.className = "sun";
-	else if (n = 1)
+	else if (n == 1)
 	  document.body.className = "mon";
-	else if (n = 2)
+	else if (n == 2)
 	  document.body.className = "tue";
-	else if (n = 3)
+	else if (n == 3)
 	  document.body.className = "wed";
-	else if (n = 4)
+	else if (n == 4)
 	  document.body.className = "thu";
-	else if (n = 5)
+	else if (n == 5)
 	  document.body.className = "fri";
 	else
 	  document.body.className = "sat";
